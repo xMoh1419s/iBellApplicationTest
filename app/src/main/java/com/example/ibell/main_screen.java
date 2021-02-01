4d090ae36bfb07f0f50fba2687f52e35283833a3
xMoh1419s <mmlibari@stu.kau.edu.sa>
2021-02-01 20:11:21 +0300

    update

--- app/src/main/java/com/example/ibell/main_screen.java
+++ app/src/main/java/com/example/ibell/main_screen.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class main_screen extends AppCompatActivity {
 
     public Button button;
+    public TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
