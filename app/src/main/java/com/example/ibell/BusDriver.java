dc4c9110701075a68d0a355d220b9c50b1934bfa
xMoh1419s <mmlibari@stu.kau.edu.sa>
2021-02-02 17:12:20 +0300

    update

---
+++ app/src/main/java/com/example/ibell/BusDriver.java
@@ -0,0 +1,15 @@
+package com.example.ibell;
+
+public class BusDriver extends User{
+
+    private String [][] schoolNames;
+
+    public void chooseSchool(String schoolNames ){
+
+    }
+    public boolean acceptPermission(){
+        return false;
+
+    }
+}
+
